a7f79de75be18c1d9e7d2a9329806ea0895fe27d
Aishwarya Panhalkar <aishwaryapanhalkar9800@gmail.com>
2021-08-17 11:24:19 +0530

    conflict resolved

--- demo/src/main/java/demo/SalesClass.java
+++ demo/src/main/java/demo/SalesClass.java
@@ -5,9 +5,11 @@ public class SalesClass {
 		int[] arr= {23,45,56,78,80};
 		int sum=0;
 		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
 			System.out.println(arr[i]);
 		}
 		System.out.println("Array size" +arr.length);
+		System.out.println("Sumation"+sum);
 	}
 	public void print() {
 		System.out.println("Print all array numbers");
